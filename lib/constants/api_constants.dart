926c46d8d9657f62c7e2ff1b22b3fa319fca88bf
Vinscent Nkomo <vinscent@skywalkinnovations.co.za>
2026-02-05 13:47:36 +0200

    update

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -4,7 +4,7 @@ class ApiConstants {
   //static const String baseUrl = "http://10.107.91.143:8000/api/pos";
 
 
-  static const String baseUrl = "https://gasman-sandbox.poscloud.co.zw/api/pos";
+  static const String baseUrl = "https://gasman.poscloud.co.zw/api/pos";
   // static const String baseUrl = "http://127.0.0.1:8000/api/pos";
 
   // Auth Endpoints
